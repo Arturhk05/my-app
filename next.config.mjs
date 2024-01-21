7a0cca3530f1c02830b1007d968ca2253ef47959
Artur Krauspenhar <arturkrauspenhar@hotmail.com>
2024-01-21 20:43:09 -0300

    Aplicação finalizada

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'store.storeimages.cdn-apple.com'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
